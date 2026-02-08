6070425ecd6b54fdb4e11602cbf69e1c2bd73c40
poswalsameer <poswalsameer2003@gmail.com>
2026-02-08 22:32:59 +0530

    updated cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://resumate.sameerposwal.xyz'],
     credentials: true,
   }),
 )
